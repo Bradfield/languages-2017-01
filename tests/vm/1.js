e76ef616343ac98303c06ae0d9aca6d3128dad12
Myles Byrne <myles@myles.id.au>
2017-01-20 18:05:55 -0800

    -m VM boilerplate

---
+++ tests/vm/1.js
@@ -0,0 +1,12 @@
+const vm = require('../../vm')
+
+// const flatten = arr => arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), [])
+
+const bytes = (strs_and_ints) => bytes
+
+vm.evalParsedObject({
+  // other 
+  code: bytes([
+    'local_load', 2
+  ])
+})
